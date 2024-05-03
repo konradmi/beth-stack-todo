a243a0c91fd6dbbc0fc34c021a60ce4c5502632c
Konrad Miekus <miekus.konrad@gmail.com>
2024-05-03 09:13:58 +0200

    setup the templating correctly

--- index.tsx
+++ index.tsx
@@ -1,17 +1,22 @@
 import { Elysia } from 'elysia'
 import { html } from '@elysiajs/html'
-import * as elements from 'typed-html'
 
-const BaseHtml = ({ children }: elements.Children) => `
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <title>Elysia</title>
-  </head>
-  ${children}
-</html>
-`
+const BaseHtml = (props: Html.PropsWithChildren) => {
+  return (
+    <>
+      {'<!doctype html>'}
+      <html lang="en">
+        <head>
+          <meta charset="UTF-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <title>TODO App</title>
+          <script src="https://unpkg.com/htmx.org@1.9.12" integrity="sha384-ujb1lZYygJmzgSwoxRggbCHcjc0rB2XoQrxeTUQyRjrOnlCoYta87iKBWq3EsdM2" crossorigin="anonymous"></script>
+        </head>
+        <body>{props.children}</body>
+      </html>
+    </>
+  );
+}
 
 const app = new Elysia()
 
@@ -20,10 +25,12 @@ app.use(html())
 app.get('/', () => (
   <BaseHtml>
     <body>
-      <h1>Hello, Elysia!!!!!!</h1>
+      <button hx-post="/clicked" hx-swap='outerHTML'>Click me</button>
     </body>
   </BaseHtml>
 ))
+
+app.post('/clicked', () => <div>I'm from the server!!!!</div>)
 
 app.listen(3000)
 console.log(`🦊 Elysia is running at on port ${app.server?.port}...`)
